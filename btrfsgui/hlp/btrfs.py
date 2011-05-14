173e020579d9f817f7fe2f6bb90c55ebe61e17e8
Hugo Mills <hugo@carfax.org.uk>
2011-05-14 21:02:50 +0100

    Add btrfs constants and a function for searching with the tree ioctl
    The tree search ioctl takes a fair amount of boilerplate code to make
    it work. Add a function to automate much of that code, and to make the
    specification of search key ranges easier.
    
    Signed-off-by: Hugo Mills <hugo@carfax.org.uk>

---
+++ btrfsgui/hlp/btrfs.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+"""btrfs constants and structure definitions
+"""
+
+import struct
+
+MINUS_ONE = 0xffffffffffffffffL
+MINUS_ONE_L = 0xffffffffL
+
+# ioctl numbers
+IOC_SPACE_INFO = 0xc0109414L
+IOC_TREE_SEARCH = 0xd0009411L
+
+# Object IDs
+DEV_ITEMS_OBJECTID = 1
+EXTENT_TREE_OBJECTID = 2
+CHUNK_TREE_OBJECTID = 3
+DEV_TREE_OBJECTID = 4
+FS_TREE_OBJECTID = 5
+ROOT_TREE_DIR_OBJECTID = 6
+CSUM_TREE_OBJECTID = 7
+ORPHAN_OBJECTID = -5
+TREE_LOG_OBJECTID = -6
+TREE_LOG_FIXUP_OBJECTID = -7
+TREE_RELOC_OBJECTID = -8
+DATA_RELOC_TREE_OBJECTID = -9
+EXTENT_CSUM_OBJECTID = -10
+FREE_SPACE_OBJECTID = -11
+MULTIPLE_OBJECTIDS = -255
+FIRST_FREE_OBJECTID = 256
+LAST_FREE_OBJECTID = -256
+FIRST_CHUNK_TREE_OBJECTID = 256
+DEV_ITEMS_OBJECTID = 1
+BTREE_INODE_OBJECTID = 1
+EMPTY_SUBVOL_DIR_OBJECTID = 2
+
+# Item keys
+INODE_ITEM_KEY = 1
+INODE_REF_KEY =	12
+XATTR_ITEM_KEY = 24
+ORPHAN_ITEM_KEY = 48
+DIR_LOG_ITEM_KEY = 60
+DIR_LOG_INDEX_KEY = 72
+DIR_ITEM_KEY = 84
+DIR_INDEX_KEY = 96
+EXTENT_DATA_KEY = 108
+EXTENT_CSUM_KEY = 128
+ROOT_ITEM_KEY = 132
+ROOT_BACKREF_KEY = 144
+ROOT_REF_KEY = 156
+EXTENT_ITEM_KEY = 168
+TREE_BLOCK_REF_KEY = 176
+EXTENT_DATA_REF_KEY = 178
+EXTENT_REF_V0_KEY = 180
+SHARED_BLOCK_REF_KEY = 182
+SHARED_DATA_REF_KEY = 184
+BLOCK_GROUP_ITEM_KEY = 192
+DEV_EXTENT_KEY = 204
+DEV_ITEM_KEY = 216
+CHUNK_ITEM_KEY = 228
+STRING_ITEM_KEY = 253
+
+# ioctl structures
+ioctl_search_key = struct.Struct("=Q6QLLL4x32x")
+ioctl_search_header = struct.Struct("=3Q2L")
+
+# Internal data structures
+dev_item = struct.Struct("<3Q3L3QL2B16s16s")
+dev_extent = struct.Struct("<4Q16s")
+chunk = struct.Struct("<4Q3L2H")
+stripe = struct.Struct("<2Q16s")
+block_group_item = struct.Struct("<3Q")
+
+def format_uuid(id):
+	return "{0:02x}{1:02x}{2:02x}{3:02x}-{4:02x}{5:02x}-{6:02x}{7:02x}-{8:02x}{9:02x}-{10:02x}{11:02x}{12:02x}{13:02x}{14:02x}{15:02x}".format(*struct.unpack("16B", id))
--- btrfsgui/hlp/size.py
+++ btrfsgui/hlp/size.py
@@ -10,8 +10,65 @@ import struct
 import json
 
 import mount
+import btrfs as btrfs
 
-BTRFS_IOC_SPACE_INFO = 0xc0109414L
+def sized_array(count):
+	return array.array("B", itertools.repeat(0, count))
+
+def search(fd, tree,
+		   objid, key_type, offset=[0, btrfs.MINUS_ONE],
+		   transid=[0, btrfs.MINUS_ONE], number=btrfs.MINUS_ONE_L,
+		   structure=None, buf=None):
+	try:
+		min_objid, max_objid = objid
+	except TypeError:
+		min_objid = max_objid = objid
+	try:
+		min_type, max_type = key_type
+	except TypeError:
+		min_type = max_type = key_type
+	try:
+		min_offset, max_offset = offset
+	except TypeError:
+		min_offset = max_offset = offset
+	try:
+		min_transid, max_transid = transid
+	except TypeError:
+		min_transid = max_transid = transid
+
+	if buf is None:
+		buf = sized_array(4096)
+	btrfs.ioctl_search_key.pack_into(
+		buf, 0,
+		tree, # Tree
+		min_objid, max_objid,		# ObjectID range
+		min_offset, max_offset,		# Offset range
+		min_transid, max_transid,	# TransID range
+		min_type, max_type,			# Key type range
+		number						# Number of items
+		)
+
+	rv = fcntl.ioctl(fd, btrfs.IOC_TREE_SEARCH, buf)
+	results = btrfs.ioctl_search_key.unpack_from(buf, 0)
+	num_items = results[9]
+	pos = btrfs.ioctl_search_key.size
+	ret = []
+	while num_items > 0:
+		num_items =- 1
+		header = btrfs.ioctl_search_header.unpack_from(buf, pos)
+		pos += btrfs.ioctl_search_header.size
+		raw_data = buf[pos:pos+header[4]]
+		data = None
+		if structure is not None:
+			data = structure.unpack_from(buf, pos)
+			#sys.stderr.write("Header claims length is {0}. Header length is {1}, structure length is {2}\n".format(header[4], btrfs.ioctl_search_header.size, structure.size))
+
+		#sys.stderr.write("Found key:\n  header {0}\n  data {1}\n".format(header, data))
+
+		ret.append((header, raw_data, data))
+		pos += header[4]
+
+	return ret
 
 def df(params, state):
 	"""Collect information on the usage of the filesystem. Replicate
@@ -22,14 +79,14 @@ def df(params, state):
 	fsfd = mount.fd(uuid)
 
 	# Get the number of spaces we need to allocate for the result
-	ret = array.array("B", [0] * 16) # Space for two __u64s
-	rv = fcntl.ioctl(fsfd, BTRFS_IOC_SPACE_INFO, ret)
+	ret = sized_array(16) # Space for two __u64s
+	rv = fcntl.ioctl(fsfd, btrfs.IOC_SPACE_INFO, ret)
 	space_slots, total_spaces = struct.unpack("QQ", ret)
 
 	# Now allocate it, and get the data
 	ret = array.array("B", [0] * (16 + 24*total_spaces))
 	struct.pack_into("QQ", ret, 0, total_spaces, 0)
-	rv = fcntl.ioctl(fsfd, BTRFS_IOC_SPACE_INFO, ret)
+	rv = fcntl.ioctl(fsfd, btrfs.IOC_SPACE_INFO, ret)
 	fsfd.close()
 
 	# Parse the result
