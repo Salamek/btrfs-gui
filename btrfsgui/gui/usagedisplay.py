51a2d194c640b7bd7c484ba0903f330f38d56eaf
Hugo Mills <hugo@carfax.org.uk>
2011-05-28 16:38:22 +0100

    Fixup idiotic syntax error in previous commit
    a385491d had a stupid syntax error, using = instead of == in an if
    statement. Fix it.
    
    Signed-off-by: Hugo Mills <hugo@carfax.org.uk>

--- btrfsgui/gui/usagedisplay.py
+++ btrfsgui/gui/usagedisplay.py
@@ -81,7 +81,7 @@ class SplitBox(object):
 		self.total = 0
 		for div in self.divisions:
 			self.total += div
-		if self.total = 0:
+		if self.total == 0:
 			self.total = 1
 
 	def set_position(self, xoffset, yoffset, width, height):
