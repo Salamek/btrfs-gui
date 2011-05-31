fdcb4fc0723b43b98eb1de7aa3e46331384a2361
Hugo Mills <hugo@carfax.org.uk>
2011-05-31 19:39:55 +0100

    Warn once on missing icons
    We already warn on stdout when there are missing icons, but we should
    put it in the user's face once, and tell them how to fix it. This
    should only show up on freshly-pulled git repos (and occasions when we
    add new icons to git). Distribution tarballs will have the pre-built
    icons in them.
    
    Signed-off-by: Hugo Mills <hugo@carfax.org.uk>

--- btrfsgui/gui/lib.py
+++ btrfsgui/gui/lib.py
@@ -5,12 +5,19 @@
 
 from tkinter import *
 from tkinter.ttk import *
+import tkinter.messagebox
+
+_icon_warned = False
 
 def image_or_blank(file):
 	"""Find and return a tkinter image that can be used as an icon
 	"""
+	global _icon_warned
 	try:
 		return PhotoImage(file=file)
 	except:
-		print("Could not find icon file {0}. You may need to run make.".format(file))
+		print("Could not find icon file {0}.".format(file))
+		if not _icon_warned:
+			tkinter.messagebox.showinfo("Missing icons", "Some icons are missing. You may need to run make in the btrfs-gui source directory to build or download them.")
+			_icon_warned = True
 		return PhotoImage()
