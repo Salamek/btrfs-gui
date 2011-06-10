3a9f99795ac0ae031c3a3dbe8cf9abe2d0a88989
Hugo Mills <hugo@carfax.org.uk>
2011-06-10 23:02:32 +0100

    Improve the block device enumeration function to return more metadata
    The GUI will need more information about which block devices are
    available, and how they relate to each other. Change the block listing
    function of the root helper to return separate records for each block
    device, with its aliases, rather than returning separate links for
    each individual inodes and link in /dev.
    
    Signed-off-by: Hugo Mills <hugo@carfax.org.uk>

--- btrfsgui/hlp/lib.py
+++ btrfsgui/hlp/lib.py
@@ -3,6 +3,10 @@
 """Exceptions and other library functions and classes
 """
 
+import os
+import os.path
+import stat
+
 class HelperException(Exception):
 	def __init__(self, msg, value=500):
 		self.message = msg
--- btrfsgui/hlp/vfs.py
+++ btrfsgui/hlp/vfs.py
@@ -81,3 +81,60 @@ def _output_file_details(typefilter, p, f):
 	sys.stdout.write(json.dumps(res))
 	sys.stdout.write("\n")
 	sys.stdout.flush()
+
+def _device_list():
+	"""Return a list of all known block devices on this system, with
+	synonyms
+	"""
+	devs = {}
+	for path, dirs, files in os.walk("/dev"):
+		# FIXME: delete all .dirs in-place from the dirs list
+
+		lastdir = os.path.basename(path)
+		for f in files:
+			if f.startswith("."):
+				continue
+			fullname = os.path.join(path, f)
+			try:
+				stats = os.stat(fullname)
+			except OSError:
+				continue
+			if not stat.S_ISBLK(stats.st_mode):
+				continue
+			dev = devs.setdefault(stats.st_rdev,
+								  {"rdev": stats.st_rdev,
+								   "alias": [],
+								   })
+			if not os.path.islink(fullname):
+				dev["cname"] = fullname
+			dev["alias"].append(fullname)
+			if lastdir == "by-id":
+				dev["by-id"] = fullname
+			if lastdir == "by-label":
+				dev["by-label"] = fullname
+			if lastdir == "by-path":
+				dev["by-path"] = fullname
+			if lastdir == "by-uuid":
+				dev["by-uuid"] = fullname
+			if path == "/dev/mapper":
+				vgname = []
+				lvname = None
+				for frag in f.split("--"):
+					if frag.find("-") != -1:
+						if lvname is not None:
+							sys.stderr.write("Malformed LVM device name {0}: ignoring LVM metadata\n".format(f))
+							break
+						vgtail, lvhead = frag.split("-")
+						vgname.append(vgtail)
+						lvname = [lvhead]
+					elif lvname is None:
+						vgname.append(frag)
+					else:
+						lvname.append(frag)
+				if lvname is not None:
+					dev["lv"] = "-".join(lvname)
+					dev["vg"] = "-".join(vgname)
+				else:
+					sys.stderr.write("LVM device name with no apparent LV part {0}: ignoring LVM metadata\n".format(f))
+
+	return devs
